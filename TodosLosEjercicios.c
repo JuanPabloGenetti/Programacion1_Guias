98083f7a4e7aa28d172217eb3a9505f07bb8f833
Juan Pablo Genetti <106981227+JuanPabloGenetti@users.noreply.github.com>
2024-04-18 20:30:30 -0300

    Matrix
    Guia de matrices

--- TodosLosEjercicios.c
+++ TodosLosEjercicios.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include "pila.c"
 #include <math.h>
+#include <string.h>
+
 
 //Prototipado guia pilas
 
 void llenarPila(Pila*, int*);
 void pasarPila(Pila A);
 void pasarPilaOrdenada(Pila A);
-int encontrarMenor(Pila* A);
+int encontrarMenor(Pila* A); // Si uso un while y lo llamo desde el main es un algoritmo de seleccion
 void insertarOrdenado(Pila *A, int new);
-void ordenamientoPorInsercion(Pila* A);
+void ordenamientoPorInsercion(Pila* A); // Llamo la funcion anterior desde esta para hacer ordenacion for insercion
 int sumarDosUltimos(Pila A);
 int sumaDeTodaLaPila(Pila A);
 int contar(Pila A);
@@ -19,6 +21,36 @@ float funcionPromedio(Pila A);
 int ejercicio10(Pila A);
 
 //Prototipado Guia Arreglos
+
+int cargarArreglo(int A[], int size);
+void mostrarArreglo(int A[], int validos);
+int sumatoria(int A[], int validos);
+void arregloA_pila(int A[], int validos);
+int encontrar(int A[], int validos, int buscar);
+void insertar_Ordenado(char A[], int *validos, char insertar);
+
+// Prototipado matrices
+// strlen( ); strcmp( );  strcmpi( ); strcpy( ); strcat( );    strstr( );  tolower( ); toupper( );     
+
+
+
+
+#define X 3
+#define Y 3
+
+#define X2 3
+#define Y2 5
+
+void cargarMatriz(int A[X][Y]);
+void mostrarMatriz(int A[X][Y]);
+void cargarRandom(int A[X][Y]);
+int suma(int A[X][Y]);
+float promedioMatriz(int A[X][Y]);
+int buscarValor(int A[X][Y], int buscar);
+int cargarArreglo_de_palabras(char B[X2][Y2]);
+int mostrarArreglo_de_palabras(char B[X2][Y2], int validos);
+int buscar(char B[X2][Y2], int validos, char a_buscar);
+
 
 
 int main() {
@@ -162,6 +194,61 @@ int main() {
             case 2:
                 printf("\nElegiste la guia de Arreglos.\n");
                 printf("Elegir ejercicio 1-10 ( 11 para volver al menu principal):\n");
+                scanf("%d", &subChoice);
+                    switch (subChoice) {
+                        case 1:
+                            printf("\n Hacer una función que reciba como parámetro un arreglo de números enteros y permita que el usuario ingrese valores al mismo por teclado. La función debe retornar la cantidad de elementos cargados en el arreglo (o pueden utilizar como puntero válidos). ");
+                            int tama = 10;
+                            int validos = 0;
+                            int arreglo[10];
+                            validos = cargarArreglo(arreglo, tama);
+                            printf("\n Los validos son: %d \n", validos);
+                        break;
+                        case 2:
+                            printf("\n Hacer una función que reciba como parámetro un arreglo y la cantidad de elementos (válidos) cargados en él y los muestre por pantalla.\n ");
+                            int arreglo2[10] = {1,2,4,5};
+                            int validos2 = 4;
+                            mostrarArreglo(arreglo2, validos2);
+                        break;
+                        case 3:
+                            printf("\n Hacer una función que reciba como parámetro un arreglo y la cantidad de elementos (válidos) cargados en él y calcule la suma de sus elementos. \n ");
+                            int arreglo3[10] = {1,2,4,5};
+                            int validos3 = 4;
+                            printf("\n La sumatoria es %d \n \n" , sumatoria(arreglo3,validos3));
+                        break;
+                        case 4:
+                            printf("\n Hacer una función que reciba como parámetro un arreglo, la cantidad de elementos (válidos) cargados en él y una Pila. La función debe copiar los elementos del arreglo en la pila. \n");
+                            int arreglo4[10] = {1,2,4,5};
+                            int validos4 = 4;
+                            arregloA_pila(arreglo4,validos4);
+                        break;
+                        case 5:
+                            printf("\n Ejercicio 5, no valia la pena hacerlo. \n ");
+                        break;
+                        case 6:
+                            printf("\n Realizar una función que indique si un elemento dado se encuentra en un arreglo de caracteres. \n ");
+                            int arreglo5[10] = {1,2,4,5};
+                            int validos5 = 4;
+                            int a_buscar = 2;
+                             int indice = encontrar(arreglo5, validos5, a_buscar);
+                              if (indice != -1) {
+                                printf("El elemento %d fue encontrado en el índice %d.\n", a_buscar, indice);
+                            } else {
+                                printf("El elemento %d no fue encontrado en el arreglo.\n", a_buscar);
+                            }
+                        break;
+                        case 7:
+                            printf("\n Realizar una función que inserte un carácter en un arreglo ordenado alfabéticamente, conservando el orden.  \n ");
+                            char arreglo6[10] = {'a', 'b', 'c', 'd'};
+                            int validos6 = 4;
+                            char a_Insertar = 'b';
+                            insertar_Ordenado(arreglo6, &validos6,a_Insertar);
+                        break;
+                        case 13:
+                        break;
+                        default:
+                        printf("\n Numero de ejercicio no valido \n");
+                    }
                 break;
             case 3:
                 printf("You chose Function C.\n");
@@ -330,4 +417,271 @@ int ejercicio10(Pila A){
 }
 
 // FINAL FUNCIONES GUIA DE PILAS // INICIO FUNCIONES GUIA DE ARREGLOS
+
+int cargarArreglo(int A[], int size){
+    int validos = 0;
+    char check = 's';
+    int i = 0;
+    
+    do{
+        printf("Ingrese un valor a su arreglo  \n");
+        scanf(" %d", &A[i]);
+        validos++;
+        printf("Desea seguir cargando el arreglo ? \n");
+        scanf(" %c", &check);
+        i++;
+    }while(validos<=size && check == 's');
+
+    return validos;
+}
+
+void mostrarArreglo(int A[], int validos){
+    for(int i=0; i<validos;i++){
+        printf("\n Posicion %d valor: %d \n \n", i , A[i]);
+    }
+}
+
+int sumatoria(int A[], int validos){
+    int i=0;
+    int suma = 0;
+    while(i<validos){
+        suma+=A[i];
+        i++;
+    }
+    return suma;
+}
+
+void arregloA_pila(int A[], int validos){
+    Pila aux; inicpila(&aux);
+    for(int i=0;i<validos;i++){
+        apilar(&aux,A[i]);
+    }
+    mostrar(&aux);
+}
+
+int encontrar(int A[], int validos, int buscar){
+    int i = 0;
+    while (i < validos) {
+        if (A[i] == buscar) {
+            return i; 
+        }
+        i++;
+    }
+    return -1;
+}
+
+void insertar_Ordenado(char A[], int *validos, char insertar) {
+
+    int i = *validos - 1; 
+
+    while (i > 0 && insertar <= A[i]) {
+        A[i + 1] = A[i]; 
+        i--;
+    }
+
+    A[i + 1] = insertar;
+   
+    (*validos)++;
+
+    int j = 0;
+
+    while (j < 10 ) {
+        printf("  %c  ", A[j]);
+        j++;
+    }
+    printf("  /n");
+}
+
+int maximo(int A[], int validos){
+
+    int maximo = A[0];
+    int i = 1;
+    while(i<validos){
+        if(maximo< A[i]){
+            maximo = A[i];
+        }
+    i++;
+    }
+return maximo;
+}
+
+int capicua(int A[], int validos) {
+    int i = 0;
+    int j = validos - 1; 
+    while (i < j) {
+        if (A[i] != A[j]) {
+            printf("No es Capicua \n");
+            return 0;
+        }
+        i++;
+        j--;
+    }
+    printf("Es Capicua \n");
+    return 1;
+}
+
+void invertir(int A[], int validos){
+    int i = 0;
+    int aux;
+    int j = validos - 1; 
+    while (i < j) { // Esta logica evita comprar indices iguales con lo cual tampoco entra el caso del arreglo impar.
+        aux = A[i];
+        A[i] = A[j];
+        A[j] = aux; 
+        i++;
+        j--;
+    }
+}
+
+void ord_Seleccion(int A[]){
+    int posmenor;
+    int i = 0;
+    int aux_realMenor = 0;
+    while(i<7){  // 7 harcodeado. O validos-1; Es validos menos 1 porque en la ultima ejecucion posMenor no va a tener contra que compararse
+        posmenor = posicionMenor(A, i);
+        aux_realMenor = A[posmenor];
+        A[posmenor] = A[i]; // Saco de la posicion del arreglo el que no va
+        A[i] = aux_realMenor; // Pongo el que si va
+        i++;    
+    }
+}
+// Podriamos decir que en la funcion seleccion, ocurre la entrega de premios a mejor explorador
+// donde los ganadores van pasando de una habitacion a otra
+// habitacion de los ganadores, en orden del mejor al peor. 
+// Especificamente esta funcion es la persona que va entre una habitacion y la otra a 
+// buscar a los ganadores.
+int posicionMenor(int A[], int pos){  // pos va a recibir 0 en la primera ejecucion y va a incrementar de a 1 subsecuentemente
+    int menor = A[pos]; // asumo que el menor es A[O]
+    int posMenor = pos; // variable auxiliar para tener el que asumo que es el menor en esta ejecucion
+    int i = pos + 1; // i va a ser todas las posiciones a partir de del que asumo que es el menor
+    while(i<8){ // hardcodeado 8, pero puedo ser los validos.
+        if( menor > A[i]){ // 1ra ejecucion. menor tiene A[0] y A[i] tiene posicion 1.
+            menor = A[i];
+            posMenor = i; // Comparo a los 2. Si el anterior menor es mas grande que el que le sigue en en el array actualizo menor y posMenor a los valores segun int i;
+        }
+    i++; // Si no se comple la condicion del while, este i++ suma hasta salir del while. Porque, por ejemplo, si posMenor 
+    } // ejecucion tras ejecucion sigue siendo el mismo, asi se sale del while sin cambiar.
+return posMenor;
+}
+
+// Podriamos decir en terminos de la vida real esta funcion (posicionMenor) es un explorador de cuevas.
+// Este lleva una unica bandera la cual clava cada vez que llega a una nueva profundidad maxima.
+// Osea la bandera sigue clavada en la ultima posicion si el explorador sigue en la
+// misma altura.
+// Al final de su aventura el explorador no vuelve con su bandera, sino con las coordenadas
+// de donde la clavo.
+
+void insertar(int A[], int ultimaPos, int valor, int validos) {
+    int i = ultimaPos;
+    while (i >= 0 && valor < A[i]) {
+        A[i + 1] = A[i];
+        i--;
+    }
+    A[i + 1] = valor;
+}
+
+void ord_Insercion(int A[], int validos) {
+    for (int i = 1; i < validos; i++) {
+        insertar(A, i - 1, A[i], validos);
+    }
+}
+
+void ord_Seleccion_Optimizado(int A[], int validos) {
+    int i, j, min_idx, temp;
+
+    for (i = 0; i < validos - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < validos; j++) {
+            if (A[j] < A[min_idx]) {
+                min_idx = j;
+            }
+        }
+
+        temp = A[i];
+        A[i] = A[min_idx];
+        A[min_idx] = temp;
+    }
+}
+
+// Guia de matrices
+
+void cargarMatriz(int A[X][Y]){
+    for(int i = 0; i<X;i++){
+        for(int j = 0; j<Y; j++){
+            printf("\n cargue valor a la matrix posicion X:%d posicion Y:%d", i , j);
+            scanf("%d", A[i][j] );
+        }
+    }
+}
+
+void mostrarMatriz(int A[X][Y]){
+    for(int i = 0; i<X;i++){
+        for(int j = 0; j<Y; j++){
+            printf("|  %d  | ", A[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+void cargarRandom(int A[X][Y]){
+    srand(time(NULL));
+    for(int i = 0; i<X;i++){
+        for(int j = 0; j<Y; j++){
+            A[i][j] = rand()%100;
+        }
+    }
+}
+
+int suma(int A[X][Y]){
+    int sumita = 0;
+    for(int i = 0; i<X;i++){
+        for(int j = 0; j<Y; j++){
+            sumita += A[i][j];
+        }
+    }
+return sumita;
+}
+
+float promedioMatriz(int A[X][Y]){
+    return float(suma(A)/float(X*Y));
+}
+
+int buscarValor(int A[X][Y], int buscar){
+    for(int i = 0; i<X;i++){
+        for(int j = 0; j<Y; j++){
+            if(buscar == A[i][j]){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int cargarArreglo_de_palabras(char B[X2][Y2]){
+    int validos = 0;
+    printf("\n cuantas palabras desea cargar ( MAX 5)");
+    fflush(stdin);
+    scanf("%d", validos);
+    for(int i = 0; i<validos-1;i++){
+        gets(B[i]);
+    }
+    return validos;
+}   
+
+int mostrarArreglo_de_palabras(char B[X2][Y2], int validos){
+    for(int i = 0; i<validos-1;i++){
+        puts(B[i]);
+    }
+}  
+
+int buscar(char B[X2][Y2], int validos, char a_buscar){
+    int i = 0;
+    while(i<validos){
+        if(strcmp(a_buscar,B[i]) == 0){
+            return 1;
+        }
+    i++; 
+    }return 0;
+}
 
